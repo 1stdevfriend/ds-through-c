bc71db915e95a131e645e84ffc80a40df4fe800d
Xhunter <kartikey@koders.in>
2022-04-30 16:04:03 +0530

    Created boilerplate in more.c

---
+++ more.c
@@ -0,0 +1,44 @@
+// More matrix operations
+// Author: Xhunter
+// Time: 03:39 PM
+// Date: 30 Apr 2022
+
+#include<stdio.h>
+#include<math.h>
+
+#define MAX 3
+
+void matrix(int [3][3]);
+void create(int [3][3]);
+void display(int [3][3]);
+void matmul(int [3][3], int[3][3], int[3][3]);
+void transpose(int[3][3], int[3][3]);
+double determinant(int [3][3]);
+int isortho(int [3][3]);
+
+int main(){
+  int mat[3][3];
+  double d;
+
+  printf("Enter elements for array:\n\n");
+  create(mat);
+
+  printf("The matrix:\n");
+  display(mat);
+
+  d = determinant(mat);
+  printf("The determinant for given matrix: %d\n\n", d);
+
+  if (d == 0)
+    printf("Matrix is singular.\n\n");
+  else
+    printf("Matrix is not singular.\n\n");
+
+  d = isortho(mat);
+  if (d != 0)
+    printf("Matrix is orthogonal.\n\n");
+  else
+    printf("Matrix is not orthogonal.\n\n");
+
+  return 0;
+}
