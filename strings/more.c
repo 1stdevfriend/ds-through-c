ed3dc22e9a76a25b004e775001655b751e0c9dfa
Xhunter <kartikey@koders.in>
2022-05-06 14:32:09 +0530

    Added boilerplate code in more.c

---
+++ strings/more.c
@@ -0,0 +1,104 @@
+// More string functions
+// Author: Xhunter
+// Date: 06 May 2022
+// Time: 02:18 PM
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int search(char *, char);
+int isequals(char *, char *);
+int issmaller(char *, char *);
+int isgreater(char *, char *);
+char* getsub(char *, int, int);
+char* leftsub(char *, int);
+char* rightsub(char *, int, int);
+void upper(char *);
+void lower(char *);
+void reverse(char *);
+int replace(char *, char, char);
+int setat(char *, char, int);
+
+int main(){
+  char s1[] = "Hello";
+  char s2[] = "Hello World";
+  char s3[] = "Four hundred thirty two";
+  char ch, *s;
+  int i;
+
+  printf("String S1: %s\n", s1);
+
+  // Check for the first occurence of a character
+  printf("Enter character to search: ");
+  scanf("%c", &ch);
+  i = search(s1, ch);
+  if (i != -1)
+    printf("The first occurence of character %c is found at index %d\n", ch, i);
+  else
+    printf("Character %c is not found in the list", ch);
+
+  printf("String s2: %s\n", s2);
+
+  // Compares two string s1 and s2
+  i = isequals(s1, s2);
+  if (i == 1)
+    printf("Strings s1 and s2 are identical");
+  else
+    printf("Strings s1 and s2 are not identical");
+
+  // Checks smaller
+  i = issmaller(s1, s2);
+  if (i == 1)
+    printf("Strings s1 is smaller than s2");
+  else
+    printf("Strings s1 is not smaller than s2");
+
+  // Checks greater
+  i = isgreater(s1, s2);
+  if (i == 1)
+    printf("Strings s1 is greater than s2");
+  else
+    printf("Strings s1 is not greater than s2");
+
+  // Extract chars at given position
+  printf("String s3: %s\n", s3);
+  s = getsub(s3, 5, 7);
+  printf("Sub string: %s\n", s);
+  free(s);
+
+  // Extract leftmost n characters
+  s = leftsub(s3, 4);
+  printf("Left sub string: %s\n", s);
+  free(s);
+
+  // Extract rightmost n characters
+  s = leftsub(s3, 4);
+  printf("Right sub string: %s\n", s);
+  free(s);
+
+  // Convert string to uppercase
+  upper(s3);
+  printf("String in upper case: %s\n", s3);
+
+  // Convert string to lowercase
+  lower(s3);
+  printf("String in lower case: %s\n", s3);
+
+  // Reverse the given string
+  reverse(s3);
+  printf("Reversed string: %s\n", s3);
+
+  // Replace first occurence of one char with new one
+  replace(s1, 'H', 'M');
+  printf("String s1: %s\n", s1);
+
+  // Sets a chat at a given position
+  i = setat(s1, 'M', 3);
+  if (i)
+    printf("String s1: %s\n", s1);
+  else
+    printf("Invalid position");
+
+  return 0;
+}
