0b19e2aa0187784555996cf13efa044b7b447b81
Xhunter <kartikey@koders.in>
2022-05-06 16:10:39 +0530

    Added fetch leftmost chars in more.c

--- strings/more.c
+++ strings/more.c
@@ -167,3 +167,17 @@ char* getsub(char *str, int spos, int n){
 
   return t;
 }
+
+// Extracts the leftmost n characters from the string
+char* leftsub(char *s, int n){
+  char *t = (char *) malloc (n + 1);
+
+  int i = 0;
+  while(i < n){
+    t[i] = *s;
+    s++; i++;
+  }
+  t[i] = '\0';
+
+  return t;
+}
